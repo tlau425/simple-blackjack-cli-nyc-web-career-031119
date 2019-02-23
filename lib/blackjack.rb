dd56ca0ac549633beecaf60ad7c11161561012cf
Tommy Lau <tlau425@yahoo.com>
2019-02-23 04:14:59 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -28,7 +28,7 @@ def end_game(card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
-def initial_round
+def initial_round()
   # code #initial_round here
   card_1 = deal_card
   card_2 = deal_card
